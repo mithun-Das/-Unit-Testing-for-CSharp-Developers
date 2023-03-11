76c8b54d3a84a3258d8b704515819de3eb92cb80
mithun-Das <mithun.das227@gmail.com>
2023-03-11 14:48:43 +0600

    Testing Strings

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var htmlFormatter = new HtmlFormatter();
+
+            var result = htmlFormatter.FormatAsBold("abc");
+
+            // Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            // More General
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
