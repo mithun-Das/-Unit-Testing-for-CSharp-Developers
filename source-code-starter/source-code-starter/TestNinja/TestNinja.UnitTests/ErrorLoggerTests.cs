97aa6d143e88290483800c379b95d617ed4ef74c
mithun-Das <mithun.das227@gmail.com>
2023-03-11 22:31:38 +0600

    Testing Methods that Throw Exceptions

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -1,5 +1,6 @@
 ﻿
 using NUnit.Framework;
+using System;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -15,6 +16,18 @@ namespace TestNinja.UnitTests
             logger.Log("a");
 
             Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() =>  logger.Log(error), Throws.ArgumentNullException);
+            //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>());
         }
     }
 }
