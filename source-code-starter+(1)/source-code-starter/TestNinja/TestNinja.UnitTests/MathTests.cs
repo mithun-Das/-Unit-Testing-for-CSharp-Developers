1320201f2e00d45581d60f1e27d90d2887fa5b10
mithun-Das <mithun.das227@gmail.com>
2023-03-11 14:45:02 +0600

    Writing Trustworthy Tests

--- source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -15,11 +15,13 @@ namespace TestNinja.UnitTests
         }
         
         [Test]
+        //[Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var result = _math.Add(1, 2);
 
-            Assert.That(result, Is.EqualTo(3));
+            // Assert.That(result, Is.EqualTo(3));
+            Assert.That(_math, Is.Not.Null);
         }
 
         [Test]
