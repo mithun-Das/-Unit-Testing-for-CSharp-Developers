3a968a1eb141e5935ac0af02c859a4945249f01d
mithun-Das <mithun.das227@gmail.com>
2023-03-12 11:27:47 +0600

    Testing Methods that Raise an Event

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -29,5 +29,18 @@ namespace TestNinja.UnitTests
             Assert.That(() =>  logger.Log(error), Throws.ArgumentNullException);
             //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>());
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
