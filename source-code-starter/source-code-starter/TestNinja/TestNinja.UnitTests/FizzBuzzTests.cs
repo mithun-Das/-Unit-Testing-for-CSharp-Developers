398c68eaa8ff961cf3b49d6395638e684f0931bb
mithun-Das <mithun.das227@gmail.com>
2023-03-12 15:37:40 +0600

    Solution- FizzBuzz

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,48 @@
+﻿
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(15)]
+        [TestCase(30)]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz(int number) 
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        [TestCase(12)]
+        [TestCase(24)]
+        public void GetOutput_InputIsDivisibleBy3_ReturnFizz(int number) 
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        [TestCase(20)]
+        public void GetOutput_InputIsDivisibleBy5_ReturnBuzz(int number) 
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        [TestCase(4)]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber(int number) 
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(number.ToString()));
+        }
+    }
+}
