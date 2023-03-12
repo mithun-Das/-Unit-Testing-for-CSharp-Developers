f336f0e1e430b269a8fd49fc7a1380017d22215c
mithun-Das <mithun.das227@gmail.com>
2023-03-13 00:18:30 +0600

    Solution- DemeritPointsCalculator

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator; 
+
+        [SetUp]
+        public void SetUp() 
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed) 
+        { 
+            Assert.That(() => _demeritPointsCalculator.CalculateDemeritPoints(speed), 
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(30, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(300,47)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritsPoints(int speed, int expectedResult) 
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+/*        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(30, 0, 0)]
+        public void CalculateDemeritPoints_WhenCalled1_ReturnDemeritsPoints(int speed, int y, int expectedResult)
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }*/
+
+    }
+}
