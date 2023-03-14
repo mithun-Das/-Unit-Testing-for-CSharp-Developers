9ccccd22bd41d65060614b4eacc9a974f8c98307
mithun-Das <mithun.das227@gmail.com>
2023-03-14 21:37:20 +0600

    Refactoring

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/VideoRepository.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+
+namespace TestNinja.Mocking
+{
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+
+    public interface IVideoRepository
+    {
+        IEnumerable<Video> GetUnprocessedVideos();
+    }
+    public class VideoRepository : IVideoRepository
+    {
+        public IEnumerable<Video> GetUnprocessedVideos()
+        {
+            using (var context = new VideoContext())
+            {
+                var videos = (from video in context.Videos
+                         where !video.IsProcessed
+                         select video).ToList();
+
+                return videos;
+            }
+
+        }
+    }
+}
--- source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
@@ -10,10 +10,12 @@ namespace TestNinja.Mocking
     public class VideoService
     {
         private IFileReader _fileReader;
+        private IVideoRepository _videoRepository;
 
-        public VideoService(IFileReader fileReader)
+        public VideoService(IFileReader fileReader, IVideoRepository videoRepository)
         {
             _fileReader = fileReader;
+            _videoRepository = videoRepository;
         }
 
         public string ReadVideoTitle()
@@ -28,19 +30,13 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
-            
-            using (var context = new VideoContext())
-            {
-                var videos = 
-                    (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
 
-                return String.Join(",", videoIds);
-            }
+            var videos = _videoRepository.GetUnprocessedVideos();
+
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
+
+            return String.Join(",", videoIds);
         }
     }
 
@@ -51,8 +47,5 @@ namespace TestNinja.Mocking
         public bool IsProcessed { get; set; }
     }
 
-    public class VideoContext : DbContext
-    {
-        public DbSet<Video> Videos { get; set; }
-    }
+
 }
