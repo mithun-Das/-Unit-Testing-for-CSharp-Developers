766d040f34031c81739ab1b3b78e081dd34cc774
mithun-Das <mithun.das227@gmail.com>
2023-03-14 11:41:14 +0600

    Refactoring Towards a Loosely-coupled Design

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace TestNinja.Mocking
+{
+    public interface IFileReader
+    {
+        string Read(string path);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            var str = File.ReadAllText("video.txt");
+
+            return str;
+        }
+    }
+}
