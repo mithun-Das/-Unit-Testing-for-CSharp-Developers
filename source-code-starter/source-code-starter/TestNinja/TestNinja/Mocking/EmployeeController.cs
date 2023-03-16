39b47a00f885a4e3caef1e7eb38805748cbd9598
mithun-Das <mithun.das227@gmail.com>
2023-03-16 22:13:54 +0600

    Refactoring EmployeeController

--- source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/EmployeeController.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/EmployeeController.cs
@@ -1,21 +1,18 @@
-﻿using System.Data.Entity;
-
+﻿
 namespace TestNinja.Mocking
 {
     public class EmployeeController
     {
-        private EmployeeContext _db;
+        private readonly IEmployeeStorage _employeeStorage;
 
-        public EmployeeController()
+        public EmployeeController(IEmployeeStorage employeeStorage)
         {
-            _db = new EmployeeContext();
+            _employeeStorage = employeeStorage;
         }
 
         public ActionResult DeleteEmployee(int id)
         {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+            _employeeStorage.DeleteEmployee(id);
             return RedirectToAction("Employees");
         }
 
@@ -28,15 +25,6 @@ namespace TestNinja.Mocking
     public class ActionResult { }
  
     public class RedirectResult : ActionResult { }
-    
-    public class EmployeeContext
-    {
-        public DbSet<Employee> Employees { get; set; }
-
-        public void SaveChanges()
-        {
-        }
-    }
 
     public class Employee
     {
