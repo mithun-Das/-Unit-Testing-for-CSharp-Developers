d763c54d7e582ba7d69ae7517cac1e48256f140d
mithun-Das <mithun.das227@gmail.com>
2023-03-16 23:39:30 +0600

    Testing EmployeeController

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿
+
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private Mock<IEmployeeStorage> _storage;
+        private EmployeeController _controller;
+
+        [SetUp] 
+        public void SetUp() 
+        {
+            _storage = new Mock<IEmployeeStorage>();
+            _controller = new EmployeeController(_storage.Object);
+        }
+
+        [Test]
+        public void DeleteEmployee_StorageCheck_ReturnActionResult()
+        {
+            var result = _controller.DeleteEmployee(1);
+
+            _storage.Verify(x => x.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployee_FunctionCheck_ReturnActionResult()
+        {
+            var result = _controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
