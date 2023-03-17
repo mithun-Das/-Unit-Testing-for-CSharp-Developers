7df193d3fb1ead17b6411f19951f9235ca64ab3c
mithun-Das <mithun.das227@gmail.com>
2023-03-17 16:51:59 +0600

    Extracting IBooking Repository

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Linq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
