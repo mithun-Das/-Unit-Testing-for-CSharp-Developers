3e3ff83853200be3fae60a98f0ed780846ac54aa
mithun-Das <mithun.das227@gmail.com>
2023-03-28 21:10:42 +0600

    Refactoring For Testability

---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/HouseKeeperRepository.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Linq;
+
+namespace TestNinja.Mocking
+{
+    public interface IHouseKeeperRepository
+    {
+        IQueryable<Housekeeper> GetHousekeepers();
+    }
+
+    public class HouseKeeperRepository : IHouseKeeperRepository
+    {
+        private static readonly UnitOfWork UnitOfWork = new UnitOfWork();
+
+        public IQueryable<Housekeeper> GetHousekeepers()
+        {
+            return UnitOfWork.Query<Housekeeper>();
+        }
+    }
+}
---
+++ source-code-starter/source-code-starter/TestNinja/TestNinja/Mocking/StatementSaver.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+using System.IO;
+
+namespace TestNinja.Mocking
+{
+    public interface IStatementSaver
+    {
+        string SaveStatement(int housekeeperOid, string housekeeperName, DateTime statementDate);
+    }
+
+    public class StatementSaver : IStatementSaver
+    {
+        public string SaveStatement(int housekeeperOid, string housekeeperName, DateTime statementDate)
+        {
+            var report = new HousekeeperStatementReport(housekeeperOid, statementDate);
+
+            if (!report.HasData)
+                return string.Empty;
+
+            report.CreateDocument();
+
+            var filename = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                string.Format("Sandpiper Statement {0:yyyy-MM} {1}.pdf", statementDate, housekeeperName));
+
+            report.ExportToPdf(filename);
+
+            return filename;
+        }
+    }
+
+    public class HousekeeperStatementReport
+    {
+        public HousekeeperStatementReport(int housekeeperOid, DateTime statementDate)
+        {
+        }
+
+        public bool HasData { get; set; }
+
+        public void CreateDocument()
+        {
+        }
+
+        public void ExportToPdf(string filename)
+        {
+        }
+    }
+}
